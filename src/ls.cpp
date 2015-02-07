84f04f323e6cd1561caa06062c0b739146030138
Michael Uy <muy003@ucr.edu>
2015-02-06 22:50:45 -0800

    Recreated initial code after git error

--- src/ls.cpp
+++ src/ls.cpp
@@ -4,15 +4,25 @@
 #include <time.h>
 #include <pwd.h>
 
-#include <syst/stat.h>
+#include <sys/stat.h>
 #include <sys/types.h>
 
 #include <unistd.h>
 #include <dirent.h>
-#include <errono.h>
+#include <errno.h>
 #include <stdio.h>
 #include <string.h>
 
 using namespace std;
 
-int
+int main(int argc,char *argv[]){
+
+    char initdirName[]={'.'};
+    char *dirName = initdirName;
+    DIR *dirp = opendir(dirName);
+    dirent *direntp;
+    while ((direntp = readdir(dirp))){
+        cout << direntp->d_name << endl;  // use stat here to find attributes of file
+    }
+    closedir(dirp);
+}
